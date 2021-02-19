ab2a46132e2f16bdce58fedfceb1dfa60206b1c7
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-02-19 23:46:01 +0100

    refactor: cambio en los objetos retornados

--- functions/commands.js
+++ functions/commands.js
@@ -2,21 +2,33 @@ const startCommand = (received) => {
   const message = 'Bienvenido a HalfonsoBot.'
   const chatid = received.chat.id
 
-  return { message, chatid }
+  return {
+    text: message,
+    chat_id: chatid,
+    method: 'sendMessage'
+  }
 }
 
 const toxicidadReply = (received) => {
   const message = 'Toxicidad fuera, mala vibra fuera 🎵🎶'
   const chatid = received.chat.id
 
-  return { message, chatid }
+  return {
+    text: message,
+    chat_id: chatid,
+    method: 'sendMessage'
+  }
 }
 
 const defaultReply = (received) => {
   const message = 'hola'
   const chatid = -1
 
-  return { message, chatid }
+  return {
+    text: message,
+    chat_id: chatid,
+    method: 'sendMessage'
+  }
 }
 
 module.exports = {
