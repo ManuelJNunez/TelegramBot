7aa71a0d459f73465c2783557ac070aebfe1526d
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-02-19 17:56:54 +0100

    Añade comandos del bot

---
+++ functions/commands.js
@@ -0,0 +1,29 @@
+
+const startCommand = (received) => {
+  const message = 'Bienvenido a HalfonsoBot.'
+  const chatid = received.chat.id
+
+  console.log('Hola :D')
+
+  return { message, chatid }
+}
+
+const toxicidadReply = (received) => {
+  const message = 'Toxicidad fuera, mala vibra fuera 🎵🎶'
+  const chatid = received.chat.id
+
+  return { message, chatid }
+}
+
+const defaultReply = (received) => {
+  const message = 'hola'
+  const chatid = -1
+
+  return { message, chatid }
+}
+
+module.exports = {
+  startCommand,
+  toxicidadReply,
+  defaultReply
+}
